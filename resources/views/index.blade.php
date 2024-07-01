3d65a3d36b71a634f185174442385ae7a1591b5a
Khant Loon Thu <142862466+KhantLoonThu@users.noreply.github.com>
2024-07-01 10:29:06 +0630

    added fonts and config

---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.master')
+@section('content')
+    <p class="font-">This is Home Page</p>
+@endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#5b3cc4',
+                success: 'rgb(23, 201, 100)',
+                danger: 'rgb(242, 19, 93)',
+                warning: 'rgb(255, 130, 0)',
+                dark: 'rgb(36, 33, 69)',
+            },
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif'],
+                openSans: ['Open Sans', 'sans-serif'],
+                roboto: ['Roboto', 'sans-serif'],
+            },
+            fontSize: {
+                'heading-1': '2.25rem', // Equivalent to 36px
+                'heading-2': '1.875rem', // Equivalent to 30px
+                'heading-3': '1.5rem', // Equivalent to 24px
+                'heading-4': '1.25rem', // Equivalent to 20px
+                'heading-5': '1.125rem', // Equivalent to 18px
+            },
+            screens: {
+                'xs': '320px',
+                'sm': '640px',
+                'md': '768px',
+                'lg': '1024px',
+                'xl': '1280px',
+            },
+        },
     },
     plugins: [],
 }
